286fc2a8198a98234bf87c151ccabf840c474b48
yamazakikakuyo <38454914+yamazakikakuyo@users.noreply.github.com>
2025-10-18 00:59:49 +0700

    Update serve_main.py

--- src/serve_main.py
+++ src/serve_main.py
@@ -3,4 +3,5 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8080"))
-    uvicorn.run("src.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
